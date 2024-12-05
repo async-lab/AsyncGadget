803d9f2ae000bec6ec3beee705207d861da434ba
dsx137 <70027572+dsx137@users.noreply.github.com>
2024-12-05 08:56:38 +0800

    feature: add update

---
+++ misc/update.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# 更新gadget
+
+##############################################
+################### META #####################
+
+MODULE_NAME="update"
+
+DIR="$(readlink -f "$(dirname "$0")")"
+export ROOT_DIR=${ROOT_DIR:-"$DIR/.."}
+
+source "$ROOT_DIR/base/STD.sh"
+
+##############################################
+################### GLOBAL ###################
+
+DEPENDED_PACKAGES=("git")
+
+##############################################
+################# TOOLFUNC ###################
+
+##############################################
+################ PROCESSFUNC #################
+
+##############################################
+################ PROGRAMFUNC #################
+
+function MAIN() {
+    if ! DEFAULT_MAIN; then
+        EXIT 1
+    fi
+
+    cd "$ROOT_DIR" &>"$STD_TMP_FILE" || true
+    local err="$(<"$STD_TMP_FILE")"
+
+    if [ -n "$err" ]; then
+        LOG "$err"
+        EXIT 1
+    fi
+
+    git pull &>"$STD_TMP_FILE" || true
+    local err="$(<"$STD_TMP_FILE")"
+
+    if [ -n "$err" ]; then
+        LOG "$err"
+        EXIT 1
+    fi
+
+    EXIT 0
+}
+
+RUN_MAIN MAIN "$@"
